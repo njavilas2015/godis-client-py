321893206e49162f9a69990b015c8ae816803d9b
Javier Avila <njavilas2015@gmail.com>
2024-12-04 20:35:13 -0300

    update

---
+++ main.py
@@ -0,0 +1,12 @@
+import socket
+
+def client(host, port, command):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((host, port))
+
+        client_socket.sendall(f"{command}\n".encode("utf-8"))
+
+        response = client_socket.recv(1024).decode("utf-8")
+
+        return response
+
